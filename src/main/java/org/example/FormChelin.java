3d4891cffa667b73e373eca28aa264be8cda54ef
Maynor <1925597@clases.edu.sv>
2025-06-04 11:29:50 -0600

    Formulario cheliiin tralalero tralala

---
+++ src/main/java/org/example/FormChelin.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.*;
+
+public class FormChelin {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton tumbaLaCasaMamiButton;
+}
