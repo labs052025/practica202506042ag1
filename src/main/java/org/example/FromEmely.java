39ed775cff0e5ac00202f60dbe10ffe0998ed969
Emely Gabriela Nájera Serrano <23emelynajera@gmail.com>
2025-06-04 11:38:03 -0600

    Formulario realizado

---
+++ src/main/java/org/example/FromEmely.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class FromEmely {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton enviarButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
