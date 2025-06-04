f2703d45da9d056989d0fd822d994de1acd92193
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-06-04 11:43:03 -0600

    mi formulario

---
+++ src/main/java/org/example/FormJoel.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javax.swing.*;
+
+public class FormJoel {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton entrarButton;
+}
