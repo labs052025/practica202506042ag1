f26ed8b168106443c5d7d2f6b9ead3e9aba549b5
Juan Carlos Vásquez López <vl25001@esfe.agape.edu.sv>
2025-06-04 11:41:48 -0600

    Cree una  rama y form

---
+++ src/main/java/org/example/JCVasquez.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class JCVasquez {
+    private JButton button1;
+    private JComboBox comboBox1;
+    private JTextField textField2;
+    private JCheckBox casadoCheckBox;
+    private JCheckBox solteroCheckBox;
+
+}
